dc9b4afe3ae24a38fcb4888ed5f14735bd12731a
darjalintu <sazonovadarja@gmail.com>
2026-03-16 00:27:19 +0300

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 alert=0
 
-if tail -n 24 sysinfo.log | awk '$4 >= 1 {alertfound = 1; exit} END {exit !alertfound}}; then
-echo "Зафиксировано в течении последних 2 минут, что loadavg1 > 1"
-alert=1
+if tail -n 24 sysinfo.log | awk '$4 >= 1 {alertfound = 1; exit} END {exit !alertfound}; then
+  echo "Зафиксировано в течении последних 2 минут, что loadavg1 > 1"
+  alert=1
 fi
 
-if tail -n 36 sysinfo.log | awk '($5 / $6) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}}'; then
-echo "Зафиксировано в течении последних 3 минут, что использовано > 60% объема оперативной памяти"
-alert=1
+if tail -n 36 sysinfo.log | awk '($5 / $6) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
+  echo "Зафиксировано в течении последних 3 минут, что использовано > 60% объема оперативной памяти"
+  alert=1
 fi
 
-if tail -n 60 sysinfo.log | awk '($7 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}}; then
-echo "Зафиксировано в течении последних 5 минут, что что использовано > 60% объема диска"
-alert=1
+if tail -n 60 sysinfo.log | awk '($7 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}; then
+  echo "Зафиксировано в течении последних 5 минут, что что использовано > 60% объема диска"
+  alert=1
 fi
 
 if [[ $alert -eq 0 ]]; then
