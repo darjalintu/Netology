ce616515ff1946f77e04448a669e5ee93873bdbf
darjalintu <sazonovadarja@gmail.com>
2026-03-16 00:43:09 +0300

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -6,12 +6,12 @@ if tail -n 24 sysinfo.log | awk '$4 >= 1 {alertfound = 1; exit} END {exit !alert
   alert=1
 fi
 
-if tail -n 36 sysinfo.log | awk '($5 / $6) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
+if tail -n 36 sysinfo.log | awk '($6 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
   echo "Зафиксировано в течении последних 3 минут, что использовано > 60% объема оперативной памяти"
   alert=1
 fi
 
-if tail -n 60 sysinfo.log | awk '($7 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
+if tail -n 60 sysinfo.log | awk '($10 / $12) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
   echo "Зафиксировано в течении последних 5 минут, что что использовано > 60% объема диска"
   alert=1
 fi
