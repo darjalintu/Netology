1f4413c466d2a2e111156b1d9a99252718d711cb
darjalintu <sazonovadarja@gmail.com>
2026-03-15 23:13:33 +0300

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-alertloadavg1=$(tail -n 24 sysinfo.log | awk 'if ($4 > 1) print $4 exit}')
-alertmem=$(tail -n 36 sysinfo.log | awk '{mem=($5/$6)*100} END {print mem}')
-alertdisk=$(tail -n 60 sysinfo.log | awk '{disk=($7/$8)*100} END {print disk}')
+alertloadavg1=$(tail -n 24 sysinfo.log | awk '{if ($4 > 1) {print 1; exit}} END {print 0}')
+alertmem=$(tail -n 36 sysinfo.log | awk '{mem = ($5 / $6) * 100; if (mem < 60) {print 1; exit}} END {print 0}')
+alertdisk=$(tail -n 60 sysinfo.log | awk '{disk = ($7 / $8) * 100; if (disk < 60) {print 1; exit}} END {print 0}')
+alert=0
 
-if [[ $alertloadavg1 < 1 ]]; then
-exit 0
-else
-echo "Зафиксировано в течении последних 2 минут, что loadavg1 > 1"
-exit 1
+if [[ $alertloadavg1 -eq 1 ]]; then
+  alert=1
+  echo "Зафиксировано в течении последних 2 минут, что loadavg1 > 1"
 fi
 
-if [[ $alertmem >= 60 ]]; then
-exit 0
-else
-echo "Зафиксировано в течении последних 3 минут, что свободный объем оперативной памяти < 60%"
-exit 1
+if [[ $alertmem -eq 1 ]]; then
+  alert=1
+  echo "Зафиксировано в течении последних 3 минут, что свободный объем оперативной памяти < 60%"
 fi
 
-if [[ $alertdisk >= 60 ]]; then
+if [[ $alertdisk -eq 1 ]]; then
+  alert=1
+  echo "Зафиксировано в течении последних 5 минут, что свободный объем на диске < 60%"
+fi
+
+if [[ $alert -eq 0 ]]; then
 exit 0
 else
-echo "Зафиксировано в течении последних 5 минут, что свободный объем на диске < 60%"
 exit 1
 fi
