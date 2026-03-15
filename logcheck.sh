3ad00939fe97782b7f6dbb90e0385e906382ccbb
darjalintu <sazonovadarja@gmail.com>
2026-03-16 00:28:42 +0300

    Update logcheck.sh

--- logcheck.sh
+++ logcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 alert=0
 
-if tail -n 24 sysinfo.log | awk '$4 >= 1 {alertfound = 1; exit} END {exit !alertfound}; then
+if tail -n 24 sysinfo.log | awk '$4 >= 1 {alertfound = 1; exit} END {exit !alertfound}'; then
   echo "Зафиксировано в течении последних 2 минут, что loadavg1 > 1"
   alert=1
 fi
@@ -11,7 +11,7 @@ if tail -n 36 sysinfo.log | awk '($5 / $6) >= 0.6 {alertfound = 1; exit} END {ex
   alert=1
 fi
 
-if tail -n 60 sysinfo.log | awk '($7 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}; then
+if tail -n 60 sysinfo.log | awk '($7 / $8) >= 0.6 {alertfound = 1; exit} END {exit !alertfound}'; then
   echo "Зафиксировано в течении последних 5 минут, что что использовано > 60% объема диска"
   alert=1
 fi
