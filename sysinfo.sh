327f5c15bd3ae08ffcc968043a75c6e4c5bc41a3
darjalintu <sazonovadarja@gmail.com>
2026-03-16 00:42:07 +0300

    Update sysinfo.sh

--- sysinfo.sh
+++ sysinfo.sh
@@ -2,10 +2,11 @@
 while true; do
   timestamp=$(date "+%b %e %H:%M:%S")
   loadavg=$(cat /proc/loadavg | awk '{print $1, " ", $2, " ", $3}')
-  memfree=$(free | awk 'NR==2 {print "Свободный объем ОЗУ:", $4}')
-  memtotal=$(free | awk 'NR==2 {print "Общий объем ОЗУ:", $2}')
-  diskfree=$(df -h / | awk 'NR==2 {print "Свободно:", $4}')
-  disktotal=$(df -h / | awk 'NR==2 {print "Общий объем:", $2}')
+  memfree=$(free | awk 'NR==2 {print "freeRAM:", $4}')
+  memtotal=$(free | awk 'NR==2 {print "fullRAM:", $2}')
+  diskfree=$(df -h / | awk 'NR==2 {print "freeDisk:", $4}')
+  disktotal=$(df -h / | awk 'NR==2 {print "fullDisk:", $2}')
   echo $timestamp $loadavg $memfree $memtotal $diskfree $disktotal >> sysinfo.log
   sleep 5
 done
+
